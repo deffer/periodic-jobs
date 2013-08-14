7bfbd3b24e0f71ccafaa2a8cddcff399708bb77e
Irina Benediktovich <i.benediktovich@auckland.ac.nz>
2013-08-15 10:21:18 +1200

    Added support for init jobs

---
+++ src/main/groovy/nz/ac/auckland/simple/InitJob.groovy
@@ -0,0 +1,12 @@
+package nz.ac.auckland.simple
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iben883
+ * Date: 15/08/13
+ * Time: 10:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface InitJob {
+
+}
