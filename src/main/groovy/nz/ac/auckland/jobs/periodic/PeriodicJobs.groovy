722da42cad131393d0a263e3e60818e6f8244655
Irina Benediktovich <i.benediktovich@auckland.ac.nz>
2014-06-17 12:43:13 +1200

    relevant comment

--- src/main/groovy/nz/ac/auckland/jobs/periodic/PeriodicJobs.groovy
+++ src/main/groovy/nz/ac/auckland/jobs/periodic/PeriodicJobs.groovy
@@ -21,21 +21,35 @@ import java.util.concurrent.TimeUnit
 import groovy.transform.CompileStatic
 
 /**
- *  Collects jobs (classes annotated with InitJob,PeriodicJob,QueuedPeriodicJob) and schedules them
- *    for execution.
+ *  Collects jobs (classes extending Job and optionally annotated with DefaultConfiguration, NamedJob, Privileged)
+ *     and schedules them for execution according to their configuration.
  *
  *  Has two execution pools: single thread and multithread.
  *
- *  InitJobs are only executed once and use single thread.
- *  QueuePeriodicJobs use single thread, which means none of the Queued jobs can run simultaneously
- *    with another Queued or Init job.  If job A has to start while other job is still running, job A will have to wait.
- *  PeriodicJobs use multithread pool. It will grow to as many treads as required to run all PeriodicJobs
+ *  Privileged jobs use single thread, which means none of the privileged jobs can run simultaneously
+ *    with another privileged job.  If job A has to start while other job is still running, job A will have to wait.
+ *
+ *  Not privileged jobs use multithread pool. It will grow to as many treads as required to run all jobs
  *    with respect to their initial and periodic delay setting.
  *
- *  Job has to be enabled (isEnabled()==true) otherwise it will not be scheduled (this is usable for instance
+ * Job with negative delay will only be invoked once, respecting privileged and initialDelay configuration.
+ *
+ * Configuration:
+ *
+ * Configuration is read from @DefaultConfiguration but can be overridden in the System properties. To enable
+ *   this override, job needs to be annotated with NamedJob and this name will be used to look up
+ *   properties.
+ *
+ * Job has to be enabled (in configuration) otherwise it will not be scheduled (this is usable for instance
  *    when some jobs has to be turned off during tests)
  *
  *  Scheduler can be turned off globally (for all jobs) by setting System property periodicJobs.enabled=false
+ *
+ *  Deprecated functionality:
+ *
+ *  This is a transition from old-style job definition (PeriodicQueuedJob, PeriodicJob, InitJob) to new style
+ *  (extends Job and annotated with @DefaultConfiguration). At the moment it supports both, but old style will be
+ *  dropped in the next release.
  *
  * author: Irina Benediktovich - http://gplus.to/IrinaBenediktovich
  */
